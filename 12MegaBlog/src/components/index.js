f13019d46dd47d07612d087518782015b94aca44
Isha Dewangan <dewanganisha05@gmail.com>
2024-04-28 22:28:20 -0700

    Mega project of complex blog

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"; 
+import RTE from './RTE';
+import Input from "./Input";
+import Select from "./Select";
+import Signup from "./SignUp";
+import Login from "./Login"; 
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,   
+    RTE,
+    Input,
+    Select,
+    Signup,
+    Login, 
+    PostForm,
+    PostCard,
+    AuthLayout
+}
